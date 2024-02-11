036a447efbb1f78d326c66d3e6741bb601c7a55a
Steven Gutierrez <stevogbmv@gmail.com>
2024-02-11 15:22:33 -0800

    Install and Create react router dom routes. Create Nav and home components for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navigation from './Navigation';
+import Home from './Home';
+// import Projects from './Projects/Projects';
+// import About from './About/About';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+  return ( 
+    <div style={{
+      backgroundImage:"url('Space.png')"}} className="w-full bg-color font-['poppins']">
+      <Navigation />
+      <Routes>
+        {/* <Route path="/about" element={<About />}/>
+        <Route path="/projects" element={<Projects />}/> */}
+        <Route path="/" element={<Home />}/>
+      </Routes>
+      
+    </div>
   )
 }
 
