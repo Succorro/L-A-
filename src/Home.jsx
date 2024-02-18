a0fa9b8be2d20beb30ec0ad045ab049185ba344a
Steven Gutierrez <stevogbmv@gmail.com>
2024-02-17 18:33:41 -0800

    Updated home page layout for better flow on information

--- src/Home.jsx
+++ src/Home.jsx
@@ -33,25 +33,25 @@ function Home() {
         <div id='nav' className="">
           <img className="absolute h-[100vh] object-cover" src="/Statue.jpg" alt="" />
         </div>
-        <h1 className="absolute text-5xl text-White font-dark ml-[16vw] sm:ml-[20vw] my-56">Luna and Associates</h1>
-        <h2 className="absolute text-3xl text-White font-dark ml-[32vw] my-72">Immigration Law</h2>
+        <div className="flex flex-col items-center">
+          <h1 className="absolute text-5xl text-White font-dark my-72">Luna and Associates</h1>
+          <h2 className="absolute text-3xl text-White font-dark my-96">Immigration Law</h2>
+        </div>
       </section>
 
-      <section id="aboutSection" className="pt-20 pb-10 bg-White flex flex-col items-center">
-        <img className="w-[50vh] mb-10" src="/lawconvo.jpg" alt="" />
-        <h1 className="text-5xl font-bold text-Blue">Susana B. Luna</h1>
-        <p className="m-10">Susana B. Luna is an experienced immigration attorney dedicated to serving clients with integrity and compassion. With an active license and a commitment to excellence, Susana brings a wealth of knowledge and expertise to her practice. Susana is fluent in Spanish, ensuring effective communication with Spanish-speaking clients.</p>
-        <img className="w-[50vh] mb-10" src="/lawconvo.jpg" alt="" />
-        <Link className="bg-Blue text-white font-semibold text-lg px-6 py-2 rounded-3xl hover:bg-blue-900 hover:text-white transition ease-in-out duration-50 delay-100" to='/team'>
-          <button >
-            Get to know the team
-          </button>
-        </Link>
-      </section>
+      <div className="h-[70vh] mt-20 md:mt-0 flex flex-row justify-between items-center ">
+        <div className="flex flex-col m-10">
+          <h1 className=" mb-10 text-3xl font-bold text-start text-slate-700">Our Experience</h1>
+          <p className="text-md text-start">Susana B. Luna is an experienced immigration attorney dedicated to serving clients with integrity and compassion. With a commitment to excellence, Susana brings a wealth of knowledge and expertise to her practice. Susana is fluent in Spanish, ensuring effective communication with Spanish-speaking clients.</p>
+          <Link className=" w-32 px-3 mt-5 py-1 mx-auto text-White rounded-2xl font-semibold bg-Blue" to='/services'><button>Services</button></Link>
+        </div>
+        <img className="w-[40vh] m-5" src="/lawconvo.jpg" alt="" />
+      </div>
 
-      <section id="servicesSection" className=" bg-white w-full">
+
+      <section id="valuesSection" className=" bg-white w-full mb-20">
         <div id="text" className="flex flex-col items-center ">
-          <span className="mt-10 mb-10 text-5xl font-bold text-Blue">Our Services</span>
+          <span className="mt-10 mb-10 text-5xl font-bold text-slate-700">Our Values</span>
         </div>
         <ul id="group" className="flex flex-col md:grid md:grid-cols-3 mx-10">
             {servicesArray.map((obj, index) => {
@@ -67,6 +67,17 @@ function Home() {
             })}
         </ul>
       </section>
+      <div className="w-full flex justify-end items-center mt-10 h-[40vh] bg-[url('/lawconvo.jpg')] bg-cover mb-40">
+        <div className="flex flex-col items-end bg-White mr-5 py-5 px-3 opacity-90">
+          <p className="w-[40vw]  text-slate-700">We are committed to protecting your rights as an immigrant and uniting your family together.</p>
+          <Link className="bg-slate-900 text-white mt-5 font-semibold mx-auto text-md px-6 py-2 rounded-3xl hover:bg-blue-900 hover:text-white transition ease-in-out duration-50 delay-100" to='/team'>
+            <button >
+              Get to know the team
+            </button>
+          </Link>
+
+        </div>
+      </div>
     </div>
   )
 }
