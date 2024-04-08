1a037d2637b14f19df7dd1830f00f30b9003fca2
Steven Gutierrez <stevogbmv@gmail.com>
2024-04-07 21:18:25 -0700

    Added more content to footer, ensured mobile and desktop variants are working correctly

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -41,24 +41,51 @@
         const currentYear = new Date().getFullYear();
       
         return (
-          <footer className="flex justify-center items-center px-16 py-20 text-base leading-7 bg-white max-md:px-5">
-            <div className="flex flex-col mt-1 max-w-full w-[502px]">
-              <nav className="flex gap-5 max-md:flex-wrap max-md:max-w-full mx-auto">
-                {navLinks.map((link) => (
-                  <NavLink key={link.label} className="group hover:cursor-pointer">
-                    {link.label}
-                  </NavLink>
-                ))}
-              </nav>
-              <div className="self-center mt-9 text-center text-slate-900 text-opacity-40 max-md:max-w-full">
-                <div className="bg-white py-4 text-center">
-                  <p className="text-sm relative">
-                    &copy; {currentYear} Susana B Luna and Associates. All rights reserved.
-                  </p>
+        <footer className="flex justify-center items-center px-16 py-20 bg-white max-md:px-5">
+            <div className="mt-6 w-full max-w-[969px] max-md:max-w-full">
+                <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+                    <section className="flex flex-col w-[35%] max-md:ml-0 max-md:w-full ">
+                        <div className="flex flex-col text-violet-950 max-md:mt-10">
+                            <h2 className="text-4xl font-bold tracking-tight text-center">
+                            Susana B Luna and Associates
+                            </h2>
+                            <p className="mt-10 text-base leading-7 text-slate-900 text-opacity-40">
+                            Insert slogan here
+                            </p>
+                            <div className="flex gap-5 justify-between mt-11 text-lg mx-auto whitespace-nowrap max-md:mt-10">
+                                <a className="w-16" href=""><img src="/instagram.png" alt="" /></a>
+                                <a className="w-16" href=""><img src="/facebook.png" alt="" /></a>
+                                <a className="w-16" href=""><img src="/tik-tok.png" alt="" /></a>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="flex flex-col ml-5 w-[65%] max-md:ml-0 max-md:w-full">
+                        <div className="grow max-md:mt-10 max-md:max-w-full">
+                            <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+                                <div className="flex justify-center items-center px-16 py-20 text-base leading-7 bg-white max-md:px-5">
+                                    <div className="flex flex-col mt-1 max-w-full w-[502px]">
+                                    <nav className="flex gap-5 max-md:flex-wrap max-md:max-w-full mx-auto">
+                                        {navLinks.map((link) => (
+                                        <NavLink key={link.label} className="group">
+                                            {link.label}
+                                        </NavLink>
+                                        ))}
+                                    </nav>
+                                    <div className="self-center mt-9 text-center text-slate-900 text-opacity-40 max-md:max-w-full">
+                                        <div className="bg-white py-4 text-center">
+                                        <p className="text-sm relative">
+                                            &copy; {currentYear} Susana B Luna and Associates. All rights reserved.
+                                        </p>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div> 
+                            </div>
+                        </div>
+                    </section>
                 </div>
-              </div>
             </div>
-          </footer>
+        </footer>
         );
       }
 // const divStyle = " mx-20 my-5 border border-0 border-b flex flex-col items-center"
