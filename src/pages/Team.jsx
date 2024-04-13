cb6ad32182113798d0f79a10a902ef1dd7b26d48
Steven Gutierrez <stevogbmv@gmail.com>
2024-04-12 20:30:47 -0700

    Inserted image for team member data

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -3,15 +3,17 @@ import TeamInfo from "@/data/TeamInfo";
 
 function Team() {
     const teamDisplay = TeamInfo.map((person, index) => {
-        console.log(person)
         return (
             <Sheet key={index}>
-                <SheetTrigger className="relative overflow-hidden md:my-10 mx-1 md:mx-3 mb-5" >
-                    <img className="w-[70vw] md:w-[30vw] object-cover" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="team member" />
+                <SheetTrigger className="relative overflow-hidden md:my-10 mx-1 md:mx-3 mb-5"  >
+                    <img className="w-[70vw] md:w-[30vw] object-cover" src={person.image} alt="team member" />
+                    <div id='content' className="absolute inset-0 flex flex-col justify-center items-center opacity-100 bg-Nav bg-opacity-0 transition-opacity duration-300 hover:opacity-100">
+                        <p className=" text-3xl mt-80 text-White">{person.name}</p>
+                    </div>
                 </SheetTrigger>
                 <SheetContent>
                     <SheetHeader className='mt-5'>
-                        <img className="w-[65vw]" src="/Statue.jpg" alt="" />
+                        <img className="h-[35vh] object-contain" src={person.image} alt="" />
                         <SheetTitle className='text-Blue text-4xl'>
                             {person.name}
                         </SheetTitle>
@@ -26,7 +28,7 @@ function Team() {
     })
   return (
     <div className=" bg-Nav pt-20 min-h-[100vh]"> 
-        <h1 className="text-White text-5xl mt-10 md:text-6xl">Team Luna</h1>
+        <h1 className="text-Blue font-bold text-5xl mt-10 md:text-6xl">Team Luna</h1>
         <section className="flex flex-col items-center p-10 text-slate-700 lg:mx-10">
             <ul className="grid grid-cols-1 md:grid-cols-3">
                 {teamDisplay}
